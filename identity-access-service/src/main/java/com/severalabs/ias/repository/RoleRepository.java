fc9f761892a6386243acfaeccb7233b258eb1334
owen_chosen <owen.chosen@gmail.com>
2025-12-29 16:35:04 +0100

    feat(repository): add user and role repositories

---
+++ identity-access-service/src/main/java/com/severalabs/ias/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.severalabs.ias.repository;
+
+import com.severalabs.ias.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ identity-access-service/src/main/java/com/severalabs/ias/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.severalabs.ias.repository;
+
+
+import com.severalabs.ias.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existByEmail(String email);
+}
