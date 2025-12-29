9011255a88b4a1803d9bc3f905421a3e4df3f8bb
owen_chosen <owen.chosen@gmail.com>
2025-12-29 09:40:30 +0100

    chore: bootstrap spring boot project

---
+++ identity-access-service/src/main/java/com/severalabs/ias/IdentityAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.severalabs.ias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityAccessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityAccessServiceApplication.class, args);
+	}
+
+}
---
+++ identity-access-service/src/test/java/com/severalabs/ias/IdentityAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.severalabs.ias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityAccessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
