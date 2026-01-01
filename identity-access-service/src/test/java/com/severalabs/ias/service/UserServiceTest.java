aadb18f169fa61b38d1107f71f1aeeb00e77b3c4
owen_chosen <owen.chosen@gmail.com>
2026-01-01 03:07:54 +0100

    test(service): update tests for password hashing and exceptions

--- identity-access-service/src/test/java/com/severalabs/ias/service/UserServiceTest.java
+++ identity-access-service/src/test/java/com/severalabs/ias/service/UserServiceTest.java
@@ -2,11 +2,13 @@ package com.severalabs.ias.service;
 
 import com.severalabs.ias.domain.User;
 import com.severalabs.ias.dto.UserRegistrationDTO;
+import com.severalabs.ias.exception.UserAlreadyExistsException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 public class UserServiceTest {
 
@@ -16,36 +18,27 @@ public class UserServiceTest {
     @Test
     void createUser_whenEmailIsUnique_shouldCreateUser() {
 
-        UserRegistrationDTO dto = new UserRegistrationDTO( );
+        UserRegistrationDTO dto = new UserRegistrationDTO();
         dto.setEmail("owen.chosen@gmail.com");
         dto.setPassword("M@kinj@1994");
-
         User savedUser = userService.createUser(dto);
 
         assertNotNull(savedUser.getId());
         assertEquals("owen.chosen@gmail.com", savedUser.getEmail());
+        assertNotEquals(dto.getPassword(), savedUser.getPassword());
 
     }
 
     @Test
     void createUser_whenEmailAlreadyExists_shouldThrowException() {
 
-        UserRegistrationDTO dto = new UserRegistrationDTO( );
-        dto.setEmail("owen.chosen@gmail.com");
-        dto.setPassword("M@kinj@1994");
+        UserRegistrationDTO dto = new UserRegistrationDTO();
+        dto.setEmail("owen.chosenc@gmail.com");
+        dto.setPassword("M@kinj@131194");
+        userService.createUser(dto);
 
-        User savedUser = userService.createUser(dto);
-
-        assertThrows(Exception.class,
+        assertThrows(UserAlreadyExistsException.class,
                 () -> userService.createUser(dto));
 
-    }
-
-    @Test
-    void findByEmail_whenUserExists_shouldReturnsUser(){
-
-        String email = "owen.chosen@gmail.com";
-
-        assertNotNull(userService.findByEmail(email));
     }
 }
