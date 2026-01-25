a77835f82b8d72abf24285e026bf069f8127f977
owen_chosen <owen.chosen@gmail.com>
2026-01-25 20:27:24 +0100

    feat(UserService): add and implement UserService login method

--- identity-access-service/src/main/java/com/severalabs/ias/service/UserServiceImpl.java
+++ identity-access-service/src/main/java/com/severalabs/ias/service/UserServiceImpl.java
@@ -27,24 +27,35 @@ public class UserServiceImpl implements UserService {
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
     private final PasswordEncoder encoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
     @Override
-    public User createUser(UserRegistrationDTO userRegistrationDTO) {
+    public User createUser(SignUpRequest signUpRequest) {
 
-        if (userRepository.existsByEmail(userRegistrationDTO.getEmail()))
+        if (userRepository.existsByEmail(signUpRequest.email()))
             throw new UserAlreadyExistsException("User Already Exists!");
 
         User newUser = new User();
         Set<Role> userRoles = newUser.getRoles();
         userRoles.add(roleRepository.findByName("USER").orElseThrow(()
                 -> new RoleNotCreatedException("Role Not Created")));
-        String encodedPassword = encoder.encode(userRegistrationDTO.getPassword());
+        String encodedPassword = encoder.encode(signUpRequest.password());
 
-        newUser.setEmail(userRegistrationDTO.getEmail());
+        newUser.setEmail(signUpRequest.email());
         newUser.setPassword(encodedPassword);
         newUser.setRoles(userRoles);
 
         return userRepository.save(newUser);
     }
 
+    @Override
+    public String loginUser(LoginRequest loginRequest) {
+
+        Authentication authObject = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.email(), loginRequest.password())
+        );
+        return jwtService.generateToken(authObject.getPrincipal().toString());
+    }
 }
