18c5e0420f9b8ba387cfa01eeab56a91c6d1cecb
owen_chosen <owen.chosen@gmail.com>
2026-02-07 22:51:33 +0100

    feat(security): add Login attempt service for protection against Brute-force attack

--- identity-access-service/src/main/java/com/severalabs/ias/security/services/LoginAttemptService.java
+++ identity-access-service/src/main/java/com/severalabs/ias/security/services/LoginAttemptService.java
@@ -1,12 +1,39 @@
-package com.severalabs.ias.service;
+package com.severalabs.ias.security.services;
 
 import com.severalabs.ias.domain.User;
+import com.severalabs.ias.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.time.Duration;
+import java.time.Instant;
 
-public interface LoginAttemptService {
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class LoginAttemptService {
 
-    Boolean isUserLocked(User user);
+    private static final int MAX_FAILED_ATTEMPTS = 5;
+    private static final Duration LOCK_DURATION = Duration.ofMinutes(10);
 
-    void loginFailed (User user);
+    public static Boolean isUserLocked(User user) {
+        return user.getLockDuration() != null
+                && user.getLockDuration().isAfter(Instant.now());
+    }
 
-    void loginPassed (User user);
+    public static User loginFailed(User user) {
+        int trials = user.getFailedLoginAttempts();
+        if ( trials >= MAX_FAILED_ATTEMPTS ) {
+            user.setFailedLoginAttempts(0);
+            user.setLockDuration(Instant.now().plus(LOCK_DURATION));
+        } else
+            user.setFailedLoginAttempts(trials + 1);
+        return user;
+    }
+
+    public static User loginPassed(User user) {
+        user.setFailedLoginAttempts(0);
+        user.setLockDuration(null);
+        return user;
+    }
 }
